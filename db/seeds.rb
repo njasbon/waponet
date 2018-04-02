0e4add744ccbae5bae21f8aa46042b3a4c717cea
njasbon@gmail.com <ec2-user@ip-172-31-7-243.us-east-2.compute.internal>
2018-04-02 17:45:03 +0000

    added to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,6 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
 
 User.create(name: 'Granger Abuhoff', password: 'password 1', email: 'grangerabuhoff@gmail.com')
 User.create(name: 'Diego  Zuluaga', password: 'password 2',email: 'diegozuluaga88@gmail.com')
@@ -15,4 +11,7 @@ User.create(name: 'Florrette Masillon', password: 'password 6',email: 'floman@ag
 User.create(name: 'Ana Cristina Stanic', password: 'password 7',email: 'acp411@hotmail.com')
 User.create(name: 'Gabriel Jasbon', password: 'password 8', password_confirmation: 'password 8',email: 'gabe08@hotmail.com')
 
+BlogPost.create(user_id: "#{User.id}", name: "Mohwawk NJ pool", address: "Mohwawk, NJ, USA")
+
+Event.create(datetime: "2018-6-6", name: "Mohwawk NJ Tournament", team_host: "Brooklyn Wets")
 
